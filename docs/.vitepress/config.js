72e69a199e729dac5006935462aebbac834a7a97
SOLBI1028 <hagkthfql@naver.com>
2022-06-26 22:50:05 +0900

    Set

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,25 @@
+export default {
+    base: '/Issue-template/',
+    title: 'Coding Platform',
+    themeConfig: {
+        nav: [
+            {
+                text: 'GitHub',
+                link: 'https://github.com/skkuding/next'
+            },
+            {
+                text: 'Page',
+                link: 'https://npc.skku.edu'
+            }
+        ],
+        sidebar: [
+            {
+                text: 'Introduction',
+                items: [
+                    { text: 'What is Coding Platform?', link: '/introduction/coding-platform'},
+                ]
+            },
+        ]
+    }
+  }
+  
